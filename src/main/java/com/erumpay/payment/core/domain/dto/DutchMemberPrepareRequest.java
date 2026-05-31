35b99ff76c89c1699d882bba6febff5f481ecd37
kdayun <rlaekdbs0329@naver.com>
2026-06-01 00:08:39 +0900

    [KAN-1330] feat: 스웨거 스키마 예시 추가

--- src/main/java/com/erumpay/payment/core/domain/dto/DutchMemberPrepareRequest.java
+++ src/main/java/com/erumpay/payment/core/domain/dto/DutchMemberPrepareRequest.java
@@ -19,21 +19,26 @@ public class DutchMemberPrepareRequest {
 
     @NotNull
     @Positive
+    @Schema(description = "결제 금액", example = "5000")
     private Long amount;
 
     @NotNull
     @Positive
+    @Schema(description = "더치 세션 ID", example = "3001")
     private Long sessionId;
 
     @NotNull
     @Positive
+    @Schema(description = "참여자 ID", example = "2001")
     private Long participantId;
 
     @NotBlank
+    @Schema(description = "주문명", example = "저녁 식사 정산")
     private String orderName;
 
     @NotNull
     @Positive
+    @Schema(description = "가맹점 ID", example = "101")
     private Long merchantId;
 
 }
