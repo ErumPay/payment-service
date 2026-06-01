4b1b0ae494decebe6b0ebf5264a4c31fc60cd77c
kdayun <rlaekdbs0329@naver.com>
2026-06-01 22:12:29 +0900

    [KAN-1138] feat: 결제 성공 시 결제 카드 기록 저장

---
+++ src/main/java/com/erumpay/payment/core/domain/dto/PaidCardRequest.java
@@ -0,0 +1,29 @@
+package com.erumpay.payment.core.domain.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PaidCardRequest {
+
+    private Long paymentId;
+    private Long pgTxnId;
+    private String pgApprovalNum;
+
+    private Long cardId;
+    private String maskedNumber;
+    private String cardName;
+    private Long paidAmount;
+    private Long discountAmount;
+    private String benefitDesc;
+    private LocalDateTime paidAt;
+}
